99cab352df8725a14079eb3ef12da6425ee1d8da
Ábia <abia.jfontes@aluno.edu.es.gov.br>
2026-08-18 11:03:00 -0300

    Create Aula02.php

---
+++ Aula02.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+    <html lang="pt-br">
+        <head> 
+        <meta charset "UTF-8">
+        </head>
+        <body>
+        <?php 
+            $nome="Ábia";
+            $idade="16";
+            echo"Meu nome é ".$nome ." e tenho ".$idade." anos";
+        ?>
+        </body>
+    </html>
