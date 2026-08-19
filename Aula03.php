d186c05658f874a896fb633260de65397942afca
Ábia <abia.jfontes@aluno.edu.es.gov.br>
2026-08-19 10:44:48 -0300

    Update Aula03.php

--- Aula03.php
+++ Aula03.php
@@ -5,24 +5,24 @@
   </head>
   <body>
     <?php
-    $preco = 120;
-    $quantidade = 3;
-    $desconto = 30;
-    $parcelas = 2;
-    $valorto = $preco * $quantidade;
-    $valordes = $valorto - $desconto;
-    $valorpar = $valordes / $parcelas;
-    $valortaxa = $valordes + 15;
-    $valoresto = $valordes % 2 ;
-    
+      $preco = 120;
+      $quantidade = 3;
+      $desconto = 30;
+      $parcelas = 2;
+      $valorto = $preco * $quantidade;
+      $valordes = $valorto - $desconto;
+      $valorpar = $valordes / $parcelas;
+      $valortaxa = $valordes + 15;
+      $valoresto = $valordes % 2;
+      
+      
     
   
-
-    echo "O valor total da compra:" .$valorto;
-    echo "<br> O valor da compra após o desconto:" .$valordes;
-    echo "<br> O valor de cada parcela: " .$valorpar;
-    echo "<br> O valor da compra caso seja acrescentada uma taxa de R$15,00: " .$valortaxa;
-    echo "<br> O resto da divisão: " .$valoresto;
+      echo "O valor total da compra:" .$valorto;
+      echo "<br> O valor da compra após o desconto:" .$valordes;
+      echo "<br> O valor de cada parcela: " .$valorpar;
+      echo "<br> O valor da compra caso seja acrescentada uma taxa de R$15,00: " .$valortaxa;
+      echo "<br> O resto da divisão: " .$valoresto;
     
 
     ?>   
